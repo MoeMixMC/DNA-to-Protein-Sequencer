5a0a6cf6dc19863c4cb09b634e63aa29d92322df
MoeMixMC <moabdalla246@gmail.com>
2014-07-11 03:59:04 -0600

    DNA-RNA-Protein.java
    Yup.. All in one class haha

---
+++ DNA-RNA-Protein.java
@@ -0,0 +1,291 @@
+package dnatoproteins;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import org.apache.commons.io.FileUtils;
+import org.biojava3.core.sequence.DNASequence;
+import org.biojava3.core.sequence.ProteinSequence;
+import org.biojava3.core.sequence.RNASequence;
+
+public class DNAtoProtein {
+	
+	  private static final String CHAR_LIST = 
+		        "ATCG";
+		    private static final int RANDOM_STRING_LENGTH = 103;
+		    
+		    
+		    public String randomSymbol(){
+		    	ArrayList<String> symbols = new ArrayList<String>();
+		    	symbols.add("+");
+		    	symbols.add("-");
+		    	Random random = new Random();
+		    	return symbols.get(random.nextInt(2)).toString();
+		    }
+		     
+		    /**
+		     * This method generates random string
+		     * @return
+		     */
+		    public String generateRandomString(){
+		         
+		        StringBuffer randStr = new StringBuffer();
+		        for(int i=0; i<RANDOM_STRING_LENGTH; i++){
+		            int number = getRandomNumber();
+		            char ch = CHAR_LIST.charAt(number);
+		            randStr.append(ch);
+		        }
+		        return randStr.toString();
+		    }
+		     
+		    /**
+		     * This method generates random numbers
+		     * @return int
+		     */
+		    private int getRandomNumber() {
+		        int randomInt = 0;
+		        Random randomGenerator = new Random();
+		        randomInt = randomGenerator.nextInt(CHAR_LIST.length());
+		        if (randomInt - 1 == -1) {
+		            return randomInt;
+		        } else {
+		            return randomInt - 1;
+		        }
+		    }
+		    
+		    public String generatemRNA(String dna){
+	    		StringBuilder sb = new StringBuilder();
+		    	if(dna.endsWith("+")){
+		    		char[] chars = dna.toCharArray();
+		    		for(int x = 0; x < chars.length; x++){
+		    			if(chars[x] == 'A'){
+		    				chars[x] = 'U';
+		    				sb.append(chars[x]);
+		    			}
+		    			else if(chars[x] == 'T'){
+		    				chars[x] = 'A';
+		    				sb.append(chars[x]);
+		    			}
+		    			else if(chars[x] == 'C'){
+		    				chars[x] = 'G';
+		    				sb.append(chars[x]);
+		    			}
+		    			else if(chars[x] == 'G'){
+		    				chars[x] = 'C';
+		    				sb.append(chars[x]);
+		    			}
+		    		}
+		    	}
+		    	if(dna.endsWith("-")){
+		    		char[] chars = dna.toCharArray();
+		    		for(int x = 0; x < chars.length; x++){
+		    			if(chars[x] == 'A'){
+		    				chars[x] = 'U';
+		    				sb.append(chars[x]);
+		    			}
+		    			else if(chars[x] == 'T'){
+		    				chars[x] = 'A';
+		    				sb.append(chars[x]);
+		    			}
+		    			else if(chars[x] == 'C'){
+		    				chars[x] = 'G';
+		    				sb.append(chars[x]);
+		    			}
+		    			else if(chars[x] == 'G'){
+		    				chars[x] = 'C';
+		    				sb.append(chars[x]);
+		    			}
+		    		}
+		    		String prerna = sb.reverse().toString();
+		    		return prerna;
+		    	}
+		    	return sb.toString();
+		    }
+		    
+		    public String[] splitStringEvery(String s, int interval) {
+		        int arrayLength = (int) Math.ceil(((s.length() / (double)interval)));
+		        String[] result = new String[arrayLength];
+
+		        int j = 0;
+		        int lastIndex = result.length - 1;
+		        for (int i = 0; i < lastIndex; i++) {
+		            result[i] = s.substring(j, j + interval);
+		            j += interval;
+		        } //Add the last bit
+		        result[lastIndex] = s.substring(j);
+
+		        return result;
+		    }
+		    
+		    private static final Map<String ,String> TRANSLATION = new HashMap<String ,String>(); 
+		    
+		    // Codons to translate 
+		    private static final String[] CODONS = { "uuu", "uuc", "uua", "uug", 
+		    "ucu", "ucc", "uca", "ucg", 
+		    "uau", "uac", "uaa", "uag", 
+		    "ugu", "ugc", "uga", "ugg", 
+		    "cuu", "cuc", "cua", "cug", 
+		    "ccu", "ccc", "cca", "ccg", 
+		    "cau", "cac", "caa", "cag", 
+		    "cgu", "cgc", "cga", "cgg", 
+		    "auu", "auc", "aua", "aug", 
+		    "acu", "acc", "aca", "acg", 
+		    "aau", "aac", "aaa", "aag", 
+		    "agu", "agc", "aga", "agg", 
+		    "guu", "guc", "gua", "gug", 
+		    "gcu", "gcc", "gca", "gcg", 
+		    "gau", "gac", "gaa", "gag", 
+		    "ggu", "ggc", "gga", "ggg" 
+		    }; 
+		    
+		    // Amino acid in map 
+		    private static final String[] AMINO_ACIDS = { "F", "F", "L", "L", 
+		    "S", "S", "S", "S", 
+		    "Y", "Y", "--STOP--", "--STOP--", 
+		    "C", "C", "--STOP--", "W", 
+		    "L", "L", "L", "L", 
+		    "P", "P", "P", "P", 
+		    "H", "H", "Q", "Q", 
+		    "R", "R", "R", "R", 
+		    "I", "I", "I", "M", 
+		    "T", "T", "T", "T", 
+		    "N", "N", "K", "K", 
+		    "S", "S", "R", "R", 
+		    "V", "V", "V", "V", 
+		    "A", "A", "A", "A", 
+		    "D", "D", "E", "E", 
+		    "G", "G", "G", "G" 
+		    };
+		    
+		    public static void init() { 
+		    	 for (int i=0; i<CODONS.length; i++) 
+		    	 TRANSLATION.put(CODONS[i], AMINO_ACIDS[i]); 
+		    }
+		    
+		    public String generateProteinSequence(String line){
+		    	String seq = new RNASequence(line).getProteinSequence().toString();
+		        return seq;
+		    }
+
+
+	public static void main(String[] args) throws IOException {		
+		
+		JOptionPane.showMessageDialog(null, "Click ok to begin");
+		
+		DNAtoProtein main = new DNAtoProtein();
+        String path = "C:"+File.separator+"DNAtoProtein"+File.separator+"input1.txt";
+        final File inputfile = new File(path);
+        
+        inputfile.createNewFile();
+        
+        FileWriter writer = new FileWriter(inputfile.getAbsoluteFile());
+        BufferedWriter bw = new BufferedWriter(writer);
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.newLine();
+        bw.write(main.generateRandomString());
+        bw.write(" "+main.randomSymbol());
+        bw.close();
+        
+        JOptionPane.showMessageDialog(null, "Random DNA strands generated successfully. Click Ok to continue.");
+        JOptionPane.showMessageDialog(null, "Click OK to begin generating the corresponding mRNA strands");
+        String outpath = "C:"+File.separator+"DNAtoProtein"+File.separator+"mRNA.txt";
+
+       
+       File output = new File(outpath);
+       output.createNewFile();
+       
+       FileWriter fw = new FileWriter(output.getAbsoluteFile());
+       BufferedWriter rna = new BufferedWriter(fw);
+       
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(0)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(1)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(2)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(3)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(4)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(5)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(6)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(7)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(8)));
+       rna.newLine();
+       rna.write(main.generatemRNA(FileUtils.readLines(inputfile).get(9)));
+       rna.close();
+       
+       JOptionPane.showMessageDialog(null, "DNA transcribed successfully!");
+       JOptionPane.showMessageDialog(null, "Click OK to begin mRNA translation to proteins");
+       
+       String proteinpath = "C:"+File.separator+"DNAtoProtein"+File.separator+"ProteinSequence.txt";
+       
+       File proteins = new File(proteinpath);
+       proteins.createNewFile();
+       
+       FileWriter lefw = new FileWriter(proteins.getAbsoluteFile());
+       BufferedWriter proteinwriter = new BufferedWriter(lefw);
+       
+       proteinwriter.write("***NOTE*** A '*' represents a STOP codon!");
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(0)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(1)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(2)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(3)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(4)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(5)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(6)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(7)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(8)));
+       proteinwriter.newLine();
+       proteinwriter.write(main.generateProteinSequence(FileUtils.readLines(output).get(9)));
+       proteinwriter.close();
+       
+       JOptionPane.showMessageDialog(null, "Protein Sequences generated succesfully!");
+       JOptionPane.showMessageDialog(null, "NOTE: A '*' represents a STOP codon.");
+	}
+
+}
